d976c9cd7b26d86502c6b956aa85ac035c1ad20c
VicenzoMF <vicenzomf@gmail.com>
2025-02-19 20:01:04 -0300

    lsp for ts, js, react

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,44 +1,82 @@
 local diagnostic_signs = require("util.icons").diagnostic_signs
+local typescript_organise_imports = require("util.lsp").typescript_organise_imports
 
 local config = function()
-  require("neoconf").setup({})
-  local cmp_nvim_lsp = require("cmp_nvim_lsp")
-  local lspconfig = require("lspconfig")
-  local capabilities = cmp_nvim_lsp.default_capabilities()
+	require("neoconf").setup({})
+	local cmp_nvim_lsp = require("cmp_nvim_lsp")
+	local lspconfig = require("lspconfig")
+	local capabilities = cmp_nvim_lsp.default_capabilities()
 
-  local luacheck = require("efmls-configs.linters.luacheck")
-  local stylua = require("efmls-configs.formatters.stylua")
-  local solhint = require("efmls-configs.linters.solhint")
-  -- local prettier_d = require("efmls-configs.formatters.prettier_d")
+	local luacheck = require("efmls-configs.linters.luacheck")
+	local stylua = require("efmls-configs.formatters.stylua")
+	local solhint = require("efmls-configs.linters.solhint")
+	local eslint = require("efmls-configs.linters.eslint")
+	-- local prettier_d = require("efmls-configs.formatters.prettier_d")
 
-  lspconfig.lua_ls.setup({
-    capabilities = capabilities,
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = {
-            vim.fn.expand("$VIMRUNTIME/lua"),
-            vim.fn.expand("$XDG_CONFIG_HOME") .. "/nvim/lua",
-          },
-        },
-      },
-    },
-  })
+	lspconfig.lua_ls.setup({
+		capabilities = capabilities,
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					library = {
+						vim.fn.expand("$VIMRUNTIME/lua"),
+						vim.fn.expand("$XDG_CONFIG_HOME") .. "/nvim/lua",
+					},
+				},
+			},
+		},
+	})
 
-  for type, icon in pairs(diagnostic_signs) do
+	-- TypesScript
+	lspconfig.ts_ls.setup({
+		capabilities = capabilities,
+		filetypes = {
+			"typescript",
+			"javascript",
+			"typescriptreact",
+			"javascriptreact",
+		},
+		commands = {
+			TypeScriptOrganizeImportants = typescript_organise_imports,
+		},
+		settings = {
+			typescript = {
+				indentStyle = "space",
+				indentSize = 2,
+			},
+		},
+	})
+
+	-- TS, JS, React, Vue
+	lspconfig.emmet_ls.setup({
+		capabilities = capabilities,
+		filetypes = {
+			"typescriptreact",
+			"javascriptreact",
+			"javascript",
+			"vue",
+		},
+	})
+
+	for type, icon in pairs(diagnostic_signs) do
 		local hl = "DiagnosticSign" .. type
 		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 	end
 
-  lspconfig.efm.setup({
-    filetypes = {
-      "lua",
-      "solidity",
-    },
-    init_options = {
+	lspconfig.efm.setup({
+		filetypes = {
+			"lua",
+			"solidity",
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+			"vue",
+		},
+		init_options = {
 			documentFormatting = true,
 			documentRangeFormatting = true,
 			hover = true,
@@ -49,10 +87,15 @@ local config = function()
 		settings = {
 			languages = {
 				lua = { luacheck, stylua },
-        solidity = { solhint }
-      },
-    }
-  })
+				solidity = { solhint },
+				typescript = { eslint },
+				javascript = { eslint },
+				typescriptreact = { eslint },
+				javascriptreact = { eslint },
+				vue = { eslint },
+			},
+		},
+	})
 end
 
 return {
@@ -62,8 +105,10 @@ return {
 	dependencies = {
 		"windwp/nvim-autopairs",
 		"williamboman/mason.nvim",
-		"creativenull/efmls-configs-nvim",		"hrsh7th/nvim-cmp",
+		"creativenull/efmls-configs-nvim",
+		"hrsh7th/nvim-cmp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-nvim-lsp",
 	},
 }
+
