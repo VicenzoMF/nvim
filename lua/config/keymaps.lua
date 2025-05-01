fa8007104886678f7fcaadb63e1031b3e8aae89d
VicenzoMF <vicenzomf@gmail.com>
2025-05-01 13:18:03 -0300

    commit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,9 @@ local mapkey = require("util.keymapper").mapvimkey
 
 mapkey("<leader>m", "NvimTreeFocus", "n") -- nvim-tree focus
 mapkey("<leader>e", "NvimTreeToggle", "n") -- nvim-tree open/close
+
+-- Telescope mappings
+mapkey("<leader>/", "Telescope live_grep", "n") -- search for string in project
+mapkey("<leader>ff", "Telescope find_files", "n") -- find files by name
+mapkey("<leader>fb", "Telescope buffers", "n") -- show open buffers
+mapkey("<leader>fh", "Telescope help_tags", "n") -- search help tags
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,40 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    { 
+      "nvim-telescope/telescope-fzf-native.nvim", 
+      build = "make" 
+    },
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    
+    telescope.setup({
+      defaults = {
+        path_display = { "truncate" },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            ["<Esc>"] = actions.close,
+          },
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
+      },
+    })
+    
+    telescope.load_extension("fzf")
+  end,
+}
